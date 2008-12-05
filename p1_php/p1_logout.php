76c50bed5654eea2141feef5a0cf218449868975
Zack Mysterio <Zack@Macintosh.local>
2008-12-05 13:06:21 -0600

    new admin page

--- p1_php/p1_logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* @author - Yogesh Mali */
-
-  setcookie("user_id","",time()-14400);
-  setcookie("user_role","",time()-14400);
-  header("Location: http://zisland.webhop.net/phase-1/p1_php/p1_login.php");
-
-?>
